fa66b9faa861af14ba1dc25b2ecfd01b23b3857c
Rory Abraham <rory@expensify.com>
2020-10-07 11:03:22 -0700

    fix Ion.clear

--- src/lib/Ion.js
+++ src/lib/Ion.js
@@ -212,10 +212,16 @@ function multiSet(data) {
 /**
  * Clear out all the data in the store
  *
- * @returns {Promise}
+ * @returns {Promise<void>}
  */
 function clear() {
-    return AsyncStorage.clear();
+    return AsyncStorage.getAllKeys()
+        .then((keys) => {
+            AsyncStorage.clear();
+            _.each(keys, (key) => {
+                keyChanged(key, null);
+            });
+        });
 }
 
 /**
