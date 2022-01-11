d3ff26da7f49ca6ed54cb6dfbe5a9b1b4397c34d
Zach Mays <zsmays@gmail.com>
2022-01-11 09:30:18 -0600

    wip

--- src/pages/iou/steps/IOUAmountPage.js
+++ src/pages/iou/steps/IOUAmountPage.js
@@ -7,16 +7,17 @@ import {
     Keyboard,
 } from 'react-native';
 import PropTypes from 'prop-types';
-import {withOnyx} from 'react-native-onyx';
+import { withOnyx } from 'react-native-onyx';
 import lodashGet from 'lodash/get';
+import _ from 'underscore';
 import ONYXKEYS from '../../../ONYXKEYS';
 import styles from '../../../styles/styles';
 import BigNumberPad from '../../../components/BigNumberPad';
-import withWindowDimensions, {windowDimensionsPropTypes} from '../../../components/withWindowDimensions';
+import withWindowDimensions, { windowDimensionsPropTypes } from '../../../components/withWindowDimensions';
 import TextInputAutoWidth from '../../../components/TextInputAutoWidth';
 import Navigation from '../../../libs/Navigation/Navigation';
 import ROUTES from '../../../ROUTES';
-import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
+import withLocalize, { withLocalizePropTypes } from '../../../components/withLocalize';
 import compose from '../../../libs/compose';
 import Button from '../../../components/Button';
 import Text from '../../../components/Text';
@@ -77,12 +78,14 @@ class IOUAmountPage extends React.Component {
 
         this.updateAmountNumberPad = this.updateAmountNumberPad.bind(this);
         this.updateAmount = this.updateAmount.bind(this);
+        this.inputSelectionChange = this.inputSelectionChange.bind(this);
         this.stripCommaFromAmount = this.stripCommaFromAmount.bind(this);
         this.focusTextInput = this.focusTextInput.bind(this);
         this.dismissKeyboardWhenBackgrounded = this.dismissKeyboardWhenBackgrounded.bind(this);
 
         this.state = {
             amount: props.selectedAmount,
+            currentSelection: { start: null, end: null },
         };
     }
 
@@ -95,6 +98,11 @@ class IOUAmountPage extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
+        if (!_.isNull(this.state.currentSelection.start)) {
+            this.textInput.setNativeProps({
+                selection: this.state.currentSelection,
+            });
+        }
         if (this.props.iou.selectedCurrencyCode === prevProps.iou.selectedCurrencyCode) {
             return;
         }
@@ -163,17 +171,59 @@ class IOUAmountPage extends React.Component {
         // Backspace button is pressed
         if (key === '<' || key === 'Backspace') {
             if (this.state.amount.length > 0) {
-                this.setState(prevState => ({
-                    amount: prevState.amount.slice(0, -1),
-                }));
+                const beginIndex = _.isNull(this.state.currentSelection.start) ? this.state.amount.length : this.state.currentSelection.start;
+                const endIndex = _.isNull(this.state.currentSelection.end) ? this.state.amount.length : this.state.currentSelection.end;
+                const [newAmount, newPosition] = this.cursorPositionToNewStringAndPositionOnBackspace(this.state.amount, beginIndex, endIndex);
+                this.setState({ amount: newAmount, currentSelection: { start: newPosition, end: newPosition } });
             }
             return;
         }
 
+
         this.setState((prevState) => {
             const amount = `${prevState.amount}${key}`;
-            return this.validateAmount(amount) ? {amount: this.stripCommaFromAmount(amount)} : prevState;
+            const valid = this.validateAmount(amount);
+            if (valid) {
+                const stripped = this.stripCommaFromAmount(amount);
+                return {
+                    amount: stripped,
+                    currentSelection: { start: stripped.length, end: stripped.length },
+                };
+            }
+            return prevState;
         });
+    }
+
+    /**
+     * take string representing current displayed input and the cursor
+     * position to calculate the displayed input and new cursor position
+     * whenever user clicks backspace
+     *
+     * @param {String} input
+     * @param {Number} start
+     * @param {Number} end
+     */
+    cursorPositionToNewStringAndPositionOnBackspace(input, start, end) {
+        // cursor is at front of input, input stays the same
+        if (start === 0 && end === 0) {
+            return [input, 0];
+        }
+
+        const inputAsArray = input.split('');
+
+        // cursor is in one location, somewhere in the input
+        if (start === end) {
+            const newInput = inputAsArray.filter((e, i) => i !== start - 1).join('');
+            const newPosition = start - 1;
+            return [newInput, newPosition];
+        }
+
+        console.log(input, start, end);
+
+        // cursor is selecting multiple elements of input
+        const newInput = inputAsArray.filter((e, i) => i <= start - 1 || i > end - 1).join('');
+        const newPosition = (end - start) - 1;
+        return [newInput, newPosition];
     }
 
     /**
@@ -187,8 +237,18 @@ class IOUAmountPage extends React.Component {
             return;
         }
 
-        this.setState({amount: this.stripCommaFromAmount(amount)});
+        this.setState({ amount: this.stripCommaFromAmount(amount) });
     }
+
+    /**
+     * Store cursor and select in local state
+     *
+     * @param {Event} event
+     */
+    inputSelectionChange(event) {
+        this.setState({ currentSelection: event.nativeEvent.selection });
+    }
+
 
     render() {
         return (
@@ -213,6 +273,8 @@ class IOUAmountPage extends React.Component {
                         inputStyle={styles.iouAmountTextInput}
                         textStyle={styles.iouAmountText}
                         onChangeText={this.updateAmount}
+                        selection={this.state.currentSelection}
+                        onSelectionChange={this.inputSelectionChange}
                         ref={el => this.textInput = el}
                         value={this.state.amount}
                         placeholder="0"
@@ -250,7 +312,7 @@ export default compose(
     withWindowDimensions,
     withLocalize,
     withOnyx({
-        currencyList: {key: ONYXKEYS.CURRENCY_LIST},
-        iou: {key: ONYXKEYS.IOU},
+        currencyList: { key: ONYXKEYS.CURRENCY_LIST },
+        iou: { key: ONYXKEYS.IOU },
     }),
 )(IOUAmountPage);
